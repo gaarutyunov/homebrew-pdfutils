842bfbf2d30bc3aa498d4eb442f29e06c28cabe3
German Arutyunov <germanarutyunov@gmail.com>
2024-04-08 13:44:38 +0400

    fix lint

--- Formula/pdfcompress.rb
+++ Formula/pdfcompress.rb
@@ -1,11 +1,11 @@
 class Pdfcompress < Formula
   desc "Useful PDF commands, such as compression, etc."
   homepage "https://github.com/gaarutyunov/homebrew-pdfutils"
-  depends_on "ghostscript"
-  url "https://github.com/gaarutyunov/homebrew-pdfutils.git", 
+  url "https://github.com/gaarutyunov/homebrew-pdfutils.git",
       tag: "0.0.1"
-  head "https://github.com/gaarutyunov/homebrew-pdfutils.git", 
+  head "https://github.com/gaarutyunov/homebrew-pdfutils.git",
        branch: "main"
+  depends_on "ghostscript"
   def install
     bin.install "pdfcompress.sh"
   end
