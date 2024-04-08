85edaaad98bda7454ba0f13b7243fc48ed279146
German Arutyunov <germanarutyunov@gmail.com>
2024-04-08 13:30:12 +0400

    fix lint errors

--- Formula/pdfcompress.rb
+++ Formula/pdfcompress.rb
@@ -1,6 +1,7 @@
-class PdfCompressFormula < Formula
+class Pdfcompress < Formula
+  desc "Useful PDF commands, such as compression, etc."
+  homepage "https://github.com/gaarutyunov/homebrew-pdfutils"
   depends_on "ghostscript"
-  
   def install
     bin.install "pdfcompress.sh"
   end
