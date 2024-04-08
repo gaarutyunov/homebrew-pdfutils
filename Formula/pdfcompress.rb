3f18b564657f742104dd83486291839a898effc1
German Arutyunov <germanarutyunov@gmail.com>
2024-04-08 13:54:36 +0400

    version

--- Formula/pdfcompress.rb
+++ Formula/pdfcompress.rb
@@ -2,7 +2,7 @@ class Pdfcompress < Formula
   desc "Useful PDF commands, such as compression, etc."
   homepage "https://github.com/gaarutyunov/homebrew-pdfutils"
   url "https://github.com/gaarutyunov/homebrew-pdfutils.git",
-      tag: "0.0.1"
+      tag: "1.0.0"
   head "https://github.com/gaarutyunov/homebrew-pdfutils.git",
        branch: "main"
   depends_on "ghostscript"
