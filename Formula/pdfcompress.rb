7be685ca61d3c2ff44b8ed9af06040f28a023c54
German Arutyunov <germanarutyunov@gmail.com>
2024-04-08 13:39:08 +0400

    added url

--- Formula/pdfcompress.rb
+++ Formula/pdfcompress.rb
@@ -2,6 +2,10 @@ class Pdfcompress < Formula
   desc "Useful PDF commands, such as compression, etc."
   homepage "https://github.com/gaarutyunov/homebrew-pdfutils"
   depends_on "ghostscript"
+  url "https://github.com/gaarutyunov/homebrew-pdfutils.git", 
+      tag: "0.0.1"
+  head "https://github.com/gaarutyunov/homebrew-pdfutils.git", 
+       branch: "main"
   def install
     bin.install "pdfcompress.sh"
   end
